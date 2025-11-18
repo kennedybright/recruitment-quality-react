805c27403fbfe3fbbc09d247cf9b3f3f20a0e58f
kennedybright <32576561+kennedybright@users.noreply.github.com>
2025-11-17 22:12:58 -0500

    Refactor admin contact exports
    Updated TECH_SUPPORT_ADMIN and CCQA_MANAGER to use environment variables.

--- src/lib/env.js
+++ src/lib/env.js
@@ -6,15 +6,7 @@ export const isDev = ENV === 'DEV'
 export const isProd = ENV === 'PROD'
 
 // Tech support admin contact for entire application. Update as needed.
-export const TECH_SUPPORT_ADMIN = {
-    name: "Kennedy Bright", 
-    email: "kennedy.bright@nielsen.com",
-    team: "GDS Performance Management"
-} // Kennedy Bright -- added 01/27/2024
+export const TECH_SUPPORT_ADMIN = process.env.TECH_SUPPORT_ADMIN // -- added 01/27/2024
 
 // Call Center Quality Manager. Update as needed.
-export const CCQA_MANAGER = {
-    name: "Joy Saldana", 
-    email: "joy.saldana@nielsen.com",
-    team: "Contact Center Quality"
-} // Joy Saldana -- added 01/27/2024
+export const CCQA_MANAGER = process.env.CCQA_MANAGER // -- added 01/27/2024
